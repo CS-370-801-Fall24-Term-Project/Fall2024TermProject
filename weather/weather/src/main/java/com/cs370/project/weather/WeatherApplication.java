a7db1882d03f71e57c33f2431018f5b3943b484b
John Maksuta <jmaksuta@colostate.edu>
2024-10-04 19:02:07 -0400

    Initial import of springboot code.

---
+++ weather/weather/src/main/java/com/cs370/project/weather/WeatherApplication.java
@@ -0,0 +1,13 @@
+package com.cs370.project.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
---
+++ weather/weather/src/test/java/com/cs370/project/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs370.project.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

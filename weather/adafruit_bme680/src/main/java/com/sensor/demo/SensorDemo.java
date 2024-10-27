39e930a32acbae2eb3f88e303d92fdf57fe530c4
jmaksuta <jmaksuta@colostate.edu>
2024-10-26 23:45:21 -0400

    Added SensorDemo class to demonstrate the sensor reading.

---
+++ weather/adafruit_bme680/src/main/java/com/sensor/demo/SensorDemo.java
@@ -0,0 +1,18 @@
+package com.sensor.demo;
+
+import com.adafruit.bme680.BME680;
+
+
+public class SensorDemo {
+
+    public static void main(String[] args) {
+
+// import board
+// import adafruit_bme680
+// i2c = board.I2C()
+        BME680 sensor = new adafruit_bme680.
+// sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+    }
+
+}

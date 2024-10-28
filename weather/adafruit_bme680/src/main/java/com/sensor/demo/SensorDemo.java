242947396d0962b53465739f6df19efdbdc48399
jmaksuta <jmaksuta@colostate.edu>
2024-10-28 00:05:35 -0400

    Finished testing the SensorDemo.

--- weather/adafruit_bme680/src/main/java/com/sensor/demo/SensorDemo.java
+++ weather/adafruit_bme680/src/main/java/com/sensor/demo/SensorDemo.java
@@ -9,18 +9,12 @@ public class SensorDemo {
     public static void main(String[] args) {
         try {
             I2CController i2c = new I2CController("bme680", 1, 0x77);
-            Adafruit_BME680 sensor = new Adafruit_BME680_I2C(i2c, 0x77, true, 100);
-            // BME680 bme680 = new BME680();
-            // Adafruit_BME680_I2C sensor = new BME680.Adafruit_BME680_I2C(i2c, 0x77, true, 100);
-            // sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+            Adafruit_BME680 sensor = new Adafruit_BME680_I2C(i2c, 0x77, false, 100);
+            
             print(String.format("Temperature: %f degrees C", sensor.temperature()));
-            // print('Temperature: {} degrees C'.format(sensor.temperature))
             print(String.format("Gas: %d ohms", sensor.gas()));
-            // print('Gas: {} ohms'.format(sensor.gas))
             print(String.format("Humidity: %f%%", sensor.humidity()));
-            // print('Humidity: {}%'.format(sensor.humidity))
             print(String.format("Pressure: %fhPa", sensor.pressure()));
-            // print('Pressure: {}hPa'.format(sensor.pressure))
 
         } catch (Exception e) {
             print(e.getMessage());
